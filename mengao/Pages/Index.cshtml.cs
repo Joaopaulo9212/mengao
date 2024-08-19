5e206dbff3714ac92594832081c07ae7c0280de0
021.898676 <021.898676@C305-237-026>
2024-08-19 13:42:16 -0300

    Adicionar arquivos de projeto.

---
+++ mengao/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using StudentApp.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+public class IndexModel : PageModel
+{
+    private readonly ApplicationDbContext _context;
+    public IndexModel(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public IList<Student> Students { get; set; }
+    public async Task OnGetAsync()
+    {
+
+        Students = await _context.Students.ToListAsync();
+    }
+}
---
+++ mengao/create.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using StudentApp.Models;
+namespace StudentApp.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        public CreateModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        [BindProperty]
+        public Student Student { get; set; }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Students.Add(Student);
+            _context.SaveChanges();
+            return RedirectToPage("./Index");
+        }
+    }
+}
